961f93c0d998545b3928c71e2dcff08822dcdc9e
Amelia Brito <ameliagiampaolo@gmail.com>
2024-10-23 15:34:19 +0200

    Add subject table

---
+++ src/app/data/services/subjects/subjects.service.ts
@@ -0,0 +1,469 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SubjectService {
+    getSubjectData() {
+        return [
+            {
+                id: '1000',
+                name: 'Castellano y Literatura I',
+                hours_per_week: 5,
+                hours_per_year: 180,
+                course: 1
+            },
+            {
+                id: '1000',
+                name: 'Castellano y Literatura II',
+                hours_per_week: 5,
+                hours_per_year: 180,
+                course: 2
+            },
+            {
+                id: '1000',
+                name: 'Castellano y Literatura III',
+                hours_per_week: 5,
+                hours_per_year: 180,
+                course: 3
+            },
+            {
+                id: '1000',
+                name: 'Castellano y Literatura IV',
+                hours_per_week: 5,
+                hours_per_year: 180,
+                course: 4
+            },
+            {
+                id: '1000',
+                name: 'Castellano y Literatura V',
+                hours_per_week: 5,
+                hours_per_year: 180,
+                course: 5
+            },
+            {
+                id: '1001',
+                name: 'Inglés I',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 1
+            },
+            {
+                id: '1001',
+                name: 'Inglés II',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 2
+            },
+            {
+                id: '1001',
+                name: 'Inglés III',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 3
+            },
+            {
+                id: '1001',
+                name: 'Inglés IV',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 4
+            },
+            {
+                id: '1001',
+                name: 'Inglés V',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 5
+            },
+            {
+                id: '1002',
+                name: 'Matemática I',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 1
+            },
+            {
+                id: '1002',
+                name: 'Matemática II',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 2
+            },
+            {
+                id: '1002',
+                name: 'Matemática III',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 3
+            },
+            {
+                id: '1002',
+                name: 'Matemática IV',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 4
+            },
+            {
+                id: '1002',
+                name: 'Matemática V',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 5
+            },
+            {
+                id: '1003',
+                name: 'Ed. Familiar y Ciud.',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 1
+            },
+            {
+                id: '1004',
+                name: 'Est. de la Naturaleza I',
+                hours_per_week: 4,
+                hours_per_year: 144,
+                course: 1
+            },
+            {
+                id: '1005',
+                name: 'Hª de Venezuela I',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 1
+            },
+            {
+                id: '1005',
+                name: 'Hª de Venezuela II',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 2
+            },
+            {
+                id: '1005',
+                name: 'Hª de Venezuela III',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 4
+            },
+            {
+                id: '1006',
+                name: 'Geografía General',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 1
+            },
+            {
+                id: '1007',
+                name: 'Educación Artística I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 1
+            },
+            {
+                id: '1007',
+                name: 'Educación Artística II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 2
+            },
+            {
+                id: '1008',
+                name: 'Ed. Física y Deporte I',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 1
+            },
+            {
+                id: '1008',
+                name: 'Ed. Física y Deporte II',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 2
+            },
+            {
+                id: '1008',
+                name: 'Ed. Física y Deporte III',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 3
+            },
+            {
+                id: '1009',
+                name: 'Inic. Dibujo Técnico (Educ. Trab.)',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 1
+            },
+            {
+                id: '1010',
+                name: 'Informática (Educ. Trab.) I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 1
+            },
+            {
+                id: '1010',
+                name: 'Informática (Educ. Trab.) II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 2
+            },
+            {
+                id: '1010',
+                name: 'Informática (Educ. Trab.) III',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 3
+            },
+            {
+                id: '1011',
+                name: 'Religión I',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 1
+            },
+            {
+                id: '1011',
+                name: 'Religión II',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 2
+            },
+            {
+                id: '1011',
+                name: 'Religión III',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 3
+            },
+            {
+                id: '1011',
+                name: 'Religión IV',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 4
+            },
+            {
+                id: '1011',
+                name: 'Religión IV',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 5
+            },
+            {
+                id: '1012',
+                name: 'Dibujo Técnico (Educ. Trab.)',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 2
+            },
+            {
+                id: '1012',
+                name: 'Dibujo Técnico Aplic. (Educ. Trab.)',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 3
+            },
+            {
+                id: '1012',
+                name: 'Historia Universal',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 2
+            },
+            {
+                id: '1013',
+                name: 'Ciencias Biológicas I',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 2
+            },
+            {
+                id: '1013',
+                name: 'Ciencias Biológicas II',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 3
+            },
+            {
+                id: '1013',
+                name: 'Ciencias Biológicas IV',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 4
+            },
+            {
+                id: '1013',
+                name: 'Ciencias Biológicas V',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 5
+            },
+            {
+                id: '1014',
+                name: 'Física I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 3
+            },
+            {
+                id: '1014',
+                name: 'Física II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 4
+            },
+            {
+                id: '1015',
+                name: 'Química I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 3
+            },
+            {
+                id: '1015',
+                name: 'Química II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 4
+            },
+            {
+                id: '1016',
+                name: 'Hª de Venezuela (C.B.)',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 3
+            },
+            {
+                id: '1017',
+                name: 'Geograf. de Venezuela I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 3
+            },
+            {
+                id: '1017',
+                name: 'Geograf. de Venezuela II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+            {
+                id: '1018',
+                name: 'Hª de Venezuela II',
+                hours_per_week: 3,
+                hours_per_year: 108,
+                course: 4
+            },
+            {
+                id: '1019',
+                name: 'Dibujo',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 4
+            },
+            {
+                id: '1020',
+                name: 'Filosofía I',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 4
+            },
+            {
+                id: '1020',
+                name: 'Filosofía II',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 5
+            },
+            {
+                id: '1021',
+                name: 'Ed. Física I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 4
+            },
+            {
+                id: '1021',
+                name: 'Ed. Física II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+            {
+                id: '1022',
+                name: 'Instrucción Premilitar I',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 4
+            },
+            {
+                id: '1022',
+                name: 'Instrucción Premilitar II',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 5
+            },
+            {
+                id: '1023',
+                name: 'Met. Investigación I',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 4
+            },
+            {
+                id: '1023',
+                name: 'Met. Investigación II',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+            {
+                id: '1024',
+                name: 'Francés',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+            {
+                id: '1025',
+                name: 'Latín y Griego',
+                hours_per_week: 1,
+                hours_per_year: 36,
+                course: 5
+            },
+            {
+                id: '1026',
+                name: 'Historia del Arte',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 4
+            },
+            {
+                id: '1027',
+                name: 'Ciencias de la Tierra',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+            {
+                id: '1028',
+                name: 'Sociología',
+                hours_per_week: 2,
+                hours_per_year: 72,
+                course: 5
+            },
+        ];
+    }
+
+    getProductsMini() {
+        return Promise.resolve(this.getSubjectData().slice(0, 5));
+    }
+
+    getProductsSmall() {
+        return Promise.resolve(this.getSubjectData().slice(0, 10));
+    }
+
+    getsubject() {
+        return Promise.resolve(this.getSubjectData());
+    }
+};
--- src/app/views/subject-management/subject-management.ts
+++ src/app/views/subject-management/subject-management.ts
@@ -1,20 +1,116 @@
 import { Component, OnInit } from '@angular/core';
-import { ConfirmationService, MessageService, SelectItemGroup } from 'primeng/api';
-import { UsersService } from '../../data/services/users/users.service'
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { AppConfig } from '../../../app/data/services/tools/app-config.service'
-
+import { SubjectService } from '../../../app/data/services/subjects/subjects.service';
+           
+export interface Subject {
+    id?: string;
+    name?: string;
+    hours_per_week?: number;
+    hours_per_year?: number;
+}
 @Component({
   selector: 'subject-management',
   templateUrl: 'subject-management.html',
   styleUrls: ['./subject-management.scss'],
-  providers: [MessageService, ConfirmationService, UsersService],
+  providers: [MessageService, ConfirmationService, SubjectService],
 })
-export class SubjectManagementComponent  {
+export class SubjectManagementComponent implements OnInit   {
     groupedCourses: any;
 
     selectedCourse: string | undefined;
 
-    constructor(private appConfig: AppConfig,) {
+    subjects!: Subject[];
+    subject!: Subject;
+    selectedSubjects!: Subject[] | null;
+
+    subjectDialog: boolean = false;
+
+    submitted: boolean = false;
+
+    constructor(private appConfig: AppConfig,
+                private subjectService: SubjectService,
+                private confirmationService: ConfirmationService,
+                private messageService: MessageService
+    ) {
         this.groupedCourses = this.appConfig.subjects;     
     }
+
+    
+  ngOnInit() {
+    this.subjectService.getsubject().then((data) => (this.subjects = data));
+  }
+
+  deleteSelectedSubjects() {
+    this.confirmationService.confirm({
+      message: '¿Está seguro de retirar las asignaturas del curso?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.subjects = this.subjects.filter(
+          (val) => !this.selectedSubjects?.includes(val)
+        );
+        this.selectedSubjects = null;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Asignaturas retiradas',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+  deleteSubject(subject: Subject) {
+    this.confirmationService.confirm({
+      message: '¿Está seguro que desea desactivar la asignatura ' + subject.name + ' del curso?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.subjects = this.subjects.filter((val) => val.id !== subject.id);
+        this.subject = {};
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Asignatura retiradas',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+
+  hideDialog() {
+    this.subjectDialog = false;
+    this.submitted = false;
+  }
+
+  saveSubject() {
+    this.submitted = true;
+
+    if (this.subject.name?.trim()) {
+        this.subject.id = this.createId();
+        this.subjects.push(this.subject);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Asignacción exitosa',
+          life: 3000,
+        });
+
+      this.subjects = [...this.subjects];
+      this.subjectDialog = false;
+      this.subject = {};
+    }
+  }
+
+  createId(): string {
+    let id = '';
+    var chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
+  }
 }
